d2cf24645cf5751ba17ddcbf91bf3644c34bddd3
pierrotm777 <pierrotm777@gmail.com>
2024-01-21 14:13:18 +0100

    Add firmwares

---
+++ MultiSwitch_Sw8/PROG/Windows/Ms8_Xany_PWM.cmd
@@ -0,0 +1,2 @@
+C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -pattiny84 -cusbasp -Pusb -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\MS8_Xany_PWM_V0_5.hex:i 
+
